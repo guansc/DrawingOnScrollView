414446cb676e08192d029f3a01aacecf62374622
Senlin Guan <guansid@gmail.com>
2014-05-17 13:26:15 +0900

    Starts from an empty iOS project

---
+++ DrawingOnScrollView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DrawingOnScrollView
+//
+//  Created by Senlin Guan on 5/17/14.
+//  Copyright (c) 2014 Senlin Guan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DrawingOnScrollView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DrawingOnScrollView
+//
+//  Created by Senlin Guan on 5/17/14.
+//  Copyright (c) 2014 Senlin Guan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
